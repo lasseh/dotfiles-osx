dc5a098bfffad87687f99018460c0a06eeaf7f66
Lasse Haugen <haugen.lasse@gmail.com>
2017-10-30 10:13:31 +0100

    removed grep options

--- .zsh/theme.zsh
+++ .zsh/theme.zsh
@@ -12,7 +12,7 @@ BASE16_SHELL="$HOME/.config/base16-shell/scripts/$SHELL_THEME.sh"
 [[ -s $BASE16_SHELL ]] && source $BASE16_SHELL || _zwarn "Could not source theme"
 
 # Grep colors
-GREP_OPTIONS='--color=auto'
-GREP_COLORS="38;5;230:sl=38;5;240:cs=38;5;100:mt=38;5;161:fn=38;5;197:ln=38;5;212:bn=38;5;44:se=38;5;166"
-export GREP_OPTIONS
-export GREP_COLORS
+#GREP_OPTIONS='--color=auto'
+#GREP_COLORS="38;5;230:sl=38;5;240:cs=38;5;100:mt=38;5;161:fn=38;5;197:ln=38;5;212:bn=38;5;44:se=38;5;166"
+#export GREP_OPTIONS
+#export GREP_COLORS
